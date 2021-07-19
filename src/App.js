10c6d369a4388e55b54858262af3ff6f89888fe6
minr2kb <kbmin1129@gmail.com>
2021-07-20 07:20:21 +0900

    20210720

--- src/App.js
+++ src/App.js
@@ -1,31 +1,67 @@
-import React from 'react';
-import './App.css';
-import Row from './components/Row';
-import requests from './requests';
-import Banner from "./components/Banner"
-import Nav from './components/Nav';
+import React from "react";
+import "./App.css";
+import Row from "./components/Row";
+import requests from "./requests";
+import Banner from "./components/Banner";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
 
 function App() {
-  return (
-    <div className="app">
-        <Nav/>
-        {/* <div style={{position: "absolute"}}> */}
-        <Banner/>
-        <div style={{position: "relative", top:"-5rem"}}>
-            <Row title="Trending Now" fetchURL={requests.fetchTrending} isLargeRow={false}/>
-            <Row title="Top Rated" fetchURL={requests.fetchTopRated} isLargeRow={false}/>
-            <Row title="Only on Netflix" fetchURL={requests.fetchNetflixOriginals} isLargeRow={true}/>
-            <Row title="Action Movies" fetchURL={requests.fetchActionMovies} isLargeRow={false}/>
-            <Row title="Comedies" fetchURL={requests.fetchComedyMovies} isLargeRow={false}/>
-            <Row title="Romantic Movies" fetchURL={requests.fetchRomanceMovies} isLargeRow={false}/>
-            <Row title="Horror Movies" fetchURL={requests.fetchHorrorMovies} isLargeRow={false}/>
-            <Row title="Sci-Fi Movies" fetchURL={requests.fetchSFMovies} isLargeRow={false}/>
-            <Row title="Animations" fetchURL={requests.fetchAnimations} isLargeRow={false}/>
-        </div>
-        {/* </div> */}
-
-    </div>
-  );
+	return (
+		<div className="app">
+			<Nav />
+			{/* <div style={{position: "absolute"}}> */}
+			<Banner />
+			<div style={{ position: "relative", top: "-5rem" }}>
+				<Row
+					title="Trending Now"
+					fetchURL={requests.fetchTrending}
+					isLargeRow={false}
+				/>
+				<Row
+					title="Top Rated"
+					fetchURL={requests.fetchTopRated}
+					isLargeRow={false}
+				/>
+				<Row
+					title="Only on Netflix"
+					fetchURL={requests.fetchNetflixOriginals}
+					isLargeRow={true}
+				/>
+				<Row
+					title="Action Movies"
+					fetchURL={requests.fetchActionMovies}
+					isLargeRow={false}
+				/>
+				<Row
+					title="Comedies"
+					fetchURL={requests.fetchComedyMovies}
+					isLargeRow={false}
+				/>
+				<Row
+					title="Romantic Movies"
+					fetchURL={requests.fetchRomanceMovies}
+					isLargeRow={false}
+				/>
+				<Row
+					title="Horror Movies"
+					fetchURL={requests.fetchHorrorMovies}
+					isLargeRow={false}
+				/>
+				<Row
+					title="Sci-Fi Movies"
+					fetchURL={requests.fetchSFMovies}
+					isLargeRow={false}
+				/>
+				<Row
+					title="Animations"
+					fetchURL={requests.fetchAnimations}
+					isLargeRow={false}
+				/>
+			</div>
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
